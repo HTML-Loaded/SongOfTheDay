641063c44dfbf26649c4922f99f32cce1bc26fbb
Leo <leonardethelion@gmail.com>
2026-04-08 01:38:33 -0500

    Implemented Login, Signup, Password Change, and Password Reset. >> >> Password change and reset files are empty and the pages use the django default. >> >> UserCreationForm used in signup doesn't include email.

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'feed',
     'social',
-    'account',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+LOGIN_REDIRECT_URL ="home"
+LOGOUT_REDIRECT_URL="home"
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
