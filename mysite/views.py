4bb0f886cc6834ab3e9c14bc82e5008f0275c520
Leo <leonardethelion@gmail.com>
2026-04-13 23:15:13 -0500

    moved sign up too mysite Extended SignUpView to now take in user email All current html files have basic formatting To Do: Allowing user to connect to their spotify account To Do: Allowing user to freind other users

---
+++ mysite/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
+from django import forms
+
+# Create your views here.
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
